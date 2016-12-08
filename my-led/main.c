ea375a2665a526a12d8c0397e2b2130e4aa58f04
GoForBroke <go.for.broke1006@gmail.com>
2016-12-09 01:24:22 +0300

    First commit

---
+++ my-led/main.c
@@ -0,0 +1,18 @@
+#define F_CPU 1000000UL  // 1 MHz
+#include <avr/io.h>
+#include <avr/delay.h>
+
+int main(void)
+{
+	int period = 5;
+
+    period = period * 1000;
+
+    DDRD = 0xff;
+    while (1) {
+        PORTD = 0xff;
+        _delay_ms(200);
+        PORTD = 0x00;
+        _delay_ms(period);
+    }
+}
