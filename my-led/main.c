926837d36f5516aa00d2fffb36678e9d7fbdea66
GoForBroke <go.for.broke1006@gmail.com>
2016-12-09 01:55:04 +0300

    First commit

--- my-led/main.c
+++ my-led/main.c
@@ -4,15 +4,11 @@
 
 int main(void)
 {
-    int period = 5;
-
-    period = period * 1000;
-
     DDRD = 0xff;
     while (1) {
         PORTD = 0xff;
         _delay_ms(200);
         PORTD = 0x00;
-        _delay_ms(period);
+        _delay_ms(5000);
     }
 }
