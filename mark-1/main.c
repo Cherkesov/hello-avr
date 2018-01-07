be72ec05fa245bded68fe22b259060b5f57b1120
goforbroke <go.for.broke1006@gmail.com>
2018-01-07 05:25:46 +0300

    Fix

--- mark-1/main.c
+++ mark-1/main.c
@@ -18,8 +18,8 @@ void move_forward(void) {
     PORTD = 0x00;
     PORTD |= (1 << PD0);
     PORTD |= (1 << PD2);
-//    PORTD |= (1 << PD4);
-//    PORTD |= (1 << PD6);
+    PORTD |= (1 << PD4);
+    PORTD |= (1 << PD6);
 }
 
 void move_backward(void) {
@@ -55,7 +55,7 @@ void rotate_right(void) {
 }
 
 int main(void) {
-    int delay = 1000;
+    int delay = 500;
 
     DDRC = 0x00; // set all C-ports as enters
     PORTC = 0b11111111;
@@ -65,8 +65,19 @@ int main(void) {
     while (1) {
         PORTD = 0x00;
 
-        if ((PINC & (1 << PC0)) == 0) {
+        char tl_signal = (PINC & (1 << PC0)) == 0;
+        char tr_signal = (PINC & (1 << PC1)) == 0;
+        char br_signal = (PINC & (1 << PC2)) == 0;
+        char bl_signal = (PINC & (1 << PC3)) == 0;
+
+        if (tl_signal && tr_signal) {
             move_forward();
+        } else if (br_signal && bl_signal) {
+            move_backward();
+        } else if (tr_signal || br_signal) {
+            rotate_right();
+        } else if (tl_signal || bl_signal) {
+            rotate_left();
         }
 
         _delay_ms(delay);
